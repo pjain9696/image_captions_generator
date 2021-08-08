e7e406c1caf9ded77a80ae1e35a4d08667ed7227
Parag Jain <pjain9696.iitr@gmail.com>
2021-08-09 00:38:24 +0530

    add feature to predict captions on custom images

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+
+from numpy.core.numeric import True_
 from module.Preprocessor import Preprocessor
 from module.Trainer import Trainer
 from utils.load_data_utils import load_config
+from utils.img_features_utils import load_image_features_on_the_fly
 
 if __name__ == '__main__':
     config = load_config()
@@ -45,6 +48,12 @@ if __name__ == '__main__':
     '''
     #4. metrics evaluation
     '''
-    bleu_df = trainer.compute_bleu_scores(config, group='val', search_method='greedy')
+    bleu_df = trainer.compute_bleu_scores(config, group='val', search_method='greedy', save_to_dir=True)
     bleu_df = trainer.compute_bleu_scores(config, group='train', search_method='greedy')
     bleu_df = trainer.compute_bleu_scores(config, group='val', search_method='beam')
+
+    #one-off predictions
+    filename = 'data/sample_images/110595925_f3395c8bd6.jpg'
+    img_tensor_val = load_image_features_on_the_fly(filename)
+    pred_cap, _ = trainer.greedy_search_pred(img_tensor_val)
+    print(f'\npredicted caption for single image = {pred_cap}\n')
